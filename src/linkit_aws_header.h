154ce4934167904487bbb5948b58e1bbced8da6a
ericwahahaha <ericwahahaha@gmail.com>
2016-03-23 06:32:24 +0800

    Forget updating header include file

--- src/linkit_aws_header.h
+++ src/linkit_aws_header.h
@@ -23,13 +23,15 @@
  #include "azure_iot_hub/httpapi.h"
  #include "azure_iot_hub/iot_logging.h"
 
- #include "mbedtls\net.h"
+ #include "mbedtls/net.h"
+ #include "mtk/NTPClient.h"
+#include "mtk/tlsconnection.h"
 
- #include "aws_iot_lib\aws_iot_mqtt_interface.h"
- #include "aws_iot_lib\aws_iot_version.h"
- #include "aws_iot_lib\aws_iot_shadow_interface.h"
- #include "aws_iot_lib\aws_iot_shadow_json_data.h"
- #include "aws_iot_lib\aws_iot_json_utils.h"
- #include "aws_iot_lib\aws_iot_log.h"
+ #include "aws_iot_lib/aws_iot_mqtt_interface.h"
+ #include "aws_iot_lib/aws_iot_version.h"
+ #include "aws_iot_lib/aws_iot_shadow_interface.h"
+ #include "aws_iot_lib/aws_iot_shadow_json_data.h"
+ #include "aws_iot_lib/aws_iot_json_utils.h"
+ #include "aws_iot_lib/aws_iot_log.h"
 
 #endif /* SRC_LINKIT_AWS_HEADER_H_ */
